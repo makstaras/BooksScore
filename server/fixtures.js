8f44233dad3ace71726640bfef87e3cec6c46a99
makstaras <makstaras@ukr.net>
2016-02-12 16:13:54 +0200

    Third microscope commit
    small additions

--- server/fixtures.js
+++ server/fixtures.js
@@ -4,19 +4,19 @@ if (Posts.find().count() === 0) {
   
   // create two users
   var tomId = Meteor.users.insert({
-    profile: { name: 'Tom Coleman' }
+    profile: { name: 'Tom' }
   });
   var tom = Meteor.users.findOne(tomId);
   var sachaId = Meteor.users.insert({
-    profile: { name: 'Sacha Greif' }
+    profile: { name: 'Sacha' }
   });
   var sacha = Meteor.users.findOne(sachaId);
   
   var telescopeId = Posts.insert({
-    title: 'Introducing Telescope',
+    title: 'Book-ye',
     userId: sacha._id,
     author: sacha.profile.name,
-    url: 'http://sachagreif.com/introducing-telescope/',
+    url: 'http://book-ye.com.ua/shop/',
     submitted: new Date(now - 7 * 3600 * 1000),
     commentsCount: 2,
     upvoters: [], votes: 0
@@ -27,7 +27,7 @@ if (Posts.find().count() === 0) {
     userId: tom._id,
     author: tom.profile.name,
     submitted: new Date(now - 5 * 3600 * 1000),
-    body: 'Interesting project Sacha, can I get involved?'
+    body: 'Very nice bookshop'
   });
   
   Comments.insert({
@@ -35,24 +35,24 @@ if (Posts.find().count() === 0) {
     userId: sacha._id,
     author: sacha.profile.name,
     submitted: new Date(now - 3 * 3600 * 1000),
-    body: 'You sure can Tom!'
+    body: 'Yes, you can find interesting books there!'
   });
   
   Posts.insert({
-    title: 'Meteor',
+    title: 'Pidruchnyky i Posibnyky',
     userId: tom._id,
     author: tom.profile.name,
-    url: 'http://meteor.com',
+    url: 'http://pp-books.com.ua/',
     submitted: new Date(now - 10 * 3600 * 1000),
     commentsCount: 0,
     upvoters: [], votes: 0
   });
   
   Posts.insert({
-    title: 'The Meteor Book',
+    title: 'Yaroslav Mudryy',
     userId: tom._id,
     author: tom.profile.name,
-    url: 'http://themeteorbook.com',
+    url: '',
     submitted: new Date(now - 12 * 3600 * 1000),
     commentsCount: 0,
     upvoters: [], votes: 0
@@ -60,7 +60,7 @@ if (Posts.find().count() === 0) {
   
   for (var i = 0; i < 10; i++) {
     Posts.insert({
-      title: 'Test post #' + i,
+      title: 'Some bookshop #' + i,
       author: sacha.profile.name,
       userId: sacha._id,
       url: 'http://google.com/?q=test-' + i,
