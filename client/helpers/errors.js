c154675b1f3a9007851a78e93ae8cddac7ad94b3
makstaras <makstaras@ukr.net>
2016-02-12 15:39:49 +0200

    Second microscope commit
    change all

--- client/helpers/errors.js
+++ client/helpers/errors.js
@@ -1,10 +1,6 @@
-Errors = new Meteor.Collection(null);
+// Local (client-only) collection
+Errors = new Mongo.Collection(null);
 
 throwError = function(message) {
-  Errors.insert({message: message, seen: false})
+  Errors.insert({message: message})
 }
-
-clearErrors = function() {
-  Errors.remove({seen: true});
-}
-
